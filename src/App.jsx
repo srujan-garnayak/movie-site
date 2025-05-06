784512a969c786bb5a5d0d3a7cfa9f966641a49c
srujan-garnayak <srujangarnayak@gmail.com>
2025-05-06 23:55:58 +0530

    imported few files from figma

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,12 @@ import React from 'react'
 const App = () => {
   return (
     <main >
+      <div className='pattern'/>
       <div className='wrapper'>
-        <header>
-          <h1>Find <span className='text-gradient'>Movies</span> You'll Enjoy Without the Hassle</h1>
+      <header>
+          <img src="public/hero.png" alt="Hero Banner" />
+          <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle</h1>
+
         </header>
 
 
